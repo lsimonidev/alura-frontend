20fdcbab2ee3077f4da4ea2a008a3c457becc4fd
Laura Simoni <laura.simoni@gmail.com>
2020-11-24 07:54:14 +0000

    listas

---
+++ 2-jsPrimeirosPassos/6-listas.js
@@ -0,0 +1,19 @@
+console.log("Trabalhando com listas");
+
+// const salvador = `Salvador`;
+// const saoPaulo = `São Paulo`;
+// const rioDeJaneiro = `Rio de Janeiro`;
+
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+);
+
+console.log("Destinos possíveis:");
+//console.log(salvador, saoPaulo, rioDeJaneiro);
+listaDeDestinos.push(`Curitiba`); // Afdicionando um item na lista
+
+console.log(listaDeDestinos);
+
+
