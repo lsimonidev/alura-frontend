3d0d58c4cd1b91b6e9d8008b6366c8b36e92f491
Laura Simoni <laura.simoni@gmail.com>
2020-11-24 07:42:46 +0000

    atribuição de variáveis - const e let

--- js-primeiros-passos/5-atribuicaoVariaveis.js
+++ js-primeiros-passos/5-atribuicaoVariaveis.js
@@ -1,5 +1,7 @@
 console.log("Atribuição de Variaveis");
 
+//boa prática: tentar manter as 
+//variaveis todas como constantes
 const idade = 36; //constante
 let nome = "Laura"; //variável
 const sobrenome = "Simoni"; //constante
@@ -8,5 +10,5 @@ console.log(idade);
 console.log(nome, sobrenome);
 console.log(`Meu nome é: ${nome} ${sobrenome}`);
 
-nome = nome + sobrenome;
-console.log(nome);
+const nomeCompleto = nome + sobrenome;
+console.log(nomeCompleto);
