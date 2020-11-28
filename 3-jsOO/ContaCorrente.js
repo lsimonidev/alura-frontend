304bf14ffc23fa8b2dde935ed100d58a9386bee6
Laura Simoni <laura.simoni@gmail.com>
2020-11-28 10:57:37 +0000

    Aula 3 - modularizando o código (npm init)

---
+++ 3-jsOO/ContaCorrente.js
@@ -0,0 +1,18 @@
+export class ContaCorrente{
+    agencia;
+    //#saldo = 0; https://github.com/tc39/proposal-class-fields#private-fields
+    _saldo = 0; // esse atributo não é privado de verdade, mas por acordo da comunidade.
+    depositar(valor){
+        if (valor <= 0) return;
+        this._saldo += valor;            
+    }
+
+    sacar(valor){
+        if(this._saldo < valor) return;
+        this._saldo -= valor;
+    }
+
+    getSaldo(){
+        return this._saldo
+    }
+}
--- 3-jsOO/index.js
+++ 3-jsOO/index.js
@@ -1,32 +1,10 @@
-class Cliente{
-    nome; 
-    cpf; 
-}
 
-class ContaCorrente{
-    agencia;
-    //#saldo = 0; https://github.com/tc39/proposal-class-fields#private-fields
-    _saldo = 0;
-    depositar(valor){
-        if (valor <= 0) return;
-        this._saldo += valor;            
-    }
-
-    sacar(valor){
-        if(this._saldo < valor) return;
-        this._saldo -= valor;
-    }
-
-    getSaldo(){
-        return this._saldo
-    }
-}
+//"npm init" no terminal para colocar o projeto como um modulo.
+import {Cliente} from "./Cliente.js"
+import {ContaCorrente} from "./ContaCorrente.js"
 
 const ccLaura = new ContaCorrente;
 ccLaura.agencia = "1001";
 ccLaura.depositar(200);
 ccLaura.depositar(-1);
 ccLaura.sacar(100);
-
-
-
