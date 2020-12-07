eddabf9fa58aabffde349df5273ebf3661fdd083
Laura Simoni <laura.simoni@gmail.com>
2020-12-07 09:04:25 +0000

    setup para aula 04 - verificando se autenticar existe e é uma função

--- 2-JavaScript/3-jsOO/SistemaAutenticacao.js
+++ 2-JavaScript/3-jsOO/SistemaAutenticacao.js
@@ -1,6 +1,10 @@
 export class SistemaAutenticacao{
     //Polimorfismo em Cliente e Funcionario
     // ser autenticável significa ter o método autenticar.
+
+    //Ducky type - possível apenas em linguagend fracamente tipadas
+    //Se anda como um pato, faz barulho de pato, deve ser um pato.
+    //não interessa o tipo do objeto e sim que ele tenha o necessário para utilizarmos.
     static login(autenticavel, senha){ 
         if(SistemaAutenticacao.ehAutenticavel(autenticavel)){
             return autenticavel.autenticar(senha);
@@ -12,6 +16,6 @@ export class SistemaAutenticacao{
     static ehAutenticavel(autenticavel){
         return "autenticar" in autenticavel &&  //Verifica se a chave existe dentro do objeto
         autenticavel.autenticar instanceof Function; // e se esta chave é uma função
-        
+
     }
 }
