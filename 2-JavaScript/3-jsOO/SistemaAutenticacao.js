82506f3acd3fa3e9b1d6c701b1500cf81664b32b
Laura Simoni <laura.simoni@gmail.com>
2020-12-07 08:54:53 +0000

    polimorfismo metodo autenticar, interface SistemaAutenticacao

--- 2-JavaScript/3-jsOO/SistemaAutenticacao.js
+++ 2-JavaScript/3-jsOO/SistemaAutenticacao.js
@@ -2,6 +2,15 @@ export class SistemaAutenticacao{
     //Polimorfismo em Cliente e Funcionario
     // ser autenticável significa ter o método autenticar.
     static login(autenticavel, senha){ 
-        return autenticavel.autenticar(senha);
+        if(SistemaAutenticacao.ehAutenticavel(autenticavel)){
+            return autenticavel.autenticar(senha);
+        }
+        return false;
+    }
+    //static: quando eu nao preciso ter uma instância para utilizar
+    //variáveis e métodos da classe
+    static ehAutenticavel(autenticavel){
+        return "autenticar" in autenticavel &&  //Verifica se a chave existe dentro do objeto
+        autenticavel.autenticar instanceof Function; // e se esta chave é uma função
     }
 }
