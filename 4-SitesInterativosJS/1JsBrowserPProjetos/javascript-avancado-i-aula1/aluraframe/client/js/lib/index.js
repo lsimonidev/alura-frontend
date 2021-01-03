b203de61e437f09d655b86470ccb695a1844e9fb
Laura Simoni <laura.simoni@gmail.com>
2021-01-03 21:20:27 +0000

    JS avançado - inicio

---
+++ 4-SitesInterativosJS/1JsBrowserPProjetos/javascript-avancado-i-aula1/aluraframe/client/js/lib/index.js
@@ -0,0 +1,16 @@
+var campos = [
+    document.querySelector('#data'),
+    document.querySelector('#quantidade'),
+    document.querySelector('#valor')
+];
+
+console.log(campos);
+
+document.querySelector('.form').addEventListener('submit',function(event){
+    var tr = document.createElement('tr');
+    campos.forEach(function(campo){
+        var td = document.createElement('td');
+        td.textContent = campo.value;
+        tr.appendChild(td);
+    });
+});
