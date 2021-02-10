1461c0fbfc9c02b750095a957922d86d22cdd450
Laura Simoni <laura.simoni@gmail.com>
2021-02-10 07:31:49 +0000

    Componente tarefa criado, armazenamento de tarefas no localStorage

--- 4-SitesInterativosJS/JS-na-web/assets/main.js
+++ 4-SitesInterativosJS/JS-na-web/assets/main.js
@@ -1,32 +1,4 @@
-import BotaoDeleta from './components/deletaTarefa.js';
-import BotaoConclui from './components/concluiTarefa.js';
-    
-const handleForm = (evento) => {
-    evento.preventDefault();
-
-    let texto = document.querySelector('[data-form-input]').value;
-    let data = moment(document.querySelector('[data-form-date]').value).format('DD/MM/YYYY');
-
-    let tarefa = {
-        data,
-        texto
-    };
-
-    addTarefa(tarefa);
-}
-
-const addTarefa = (tarefa) => {
-    //Cria item
-    const item = document.createElement('li');
-    item.innerHTML = `<p> ${tarefa.data} - ${tarefa.texto}</p>`;
-    item.classList.add('task');
-    item.appendChild(BotaoConclui());
-    item.appendChild(BotaoDeleta());
-
-    //Adiciona item à lista de tarefas
-    const lista = document.querySelector('[data-list]');
-    lista.appendChild(item);
-}
+import {handleForm} from './components/criaTarefa.js';
 
 const button = document.querySelector('[data-form-button]');
 button.addEventListener('click', handleForm);
